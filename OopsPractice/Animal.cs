0d1600f49c03e7b78d535b42d920126059b9cf67
kiran-sasaki <kiransekar1111@gmail.com>
2022-12-27 15:36:07 +0530

    [Kiran] Added Single Level Inheritance in Oops Practice Problem

---
+++ OopsPractice/Animal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsPractice
+{
+    public class Animal
+    {
+        public void Type()
+        {
+            Console.WriteLine("Type of Animal");
+        }
+    }
+    public class Mammals:Animal
+    {
+        public void Dog()
+        {
+            Console.WriteLine("Dog is A mammal");
+        }
+    }
+}
